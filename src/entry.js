99e67fe28cb5cdbdc4b9bb83c350c4918caf46f4
alina <smaylik2410@gmail.com>
2017-01-05 14:49:34 +0200

    Add eslint

--- src/entry.js
+++ src/entry.js
@@ -1,15 +1,16 @@
-require('jquery');
-require('bootstrap');
+require('jquery'); //eslint-disable-line
+require('bootstrap'); // eslint-disable-line
 
 require('../bower_components/bootstrap/less/bootstrap.less');
 require('./less/all.less');
 
-document.addEventListener("DOMContentLoaded", function () {
-    const utils = require('jsmp-infra-string-utils');
-    const greeting = document.querySelector('#greeting');
+const utils = require('jsmp-infra-string-utils');
 
-    greeting.addEventListener('click', function () {
-        console.log(utils.modify('hello, user!!!', 'upper'));
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  const greeting = document.querySelector('#greeting');
+
+  greeting.addEventListener('click', () => {
+    console.log(utils.modify('hello, user!!!', 'upper'));
+  });
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,75 +2,77 @@ const path = require('path');
 const webpack = require('webpack');
 const SpritesmithPlugin = require('webpack-spritesmith');
 const CopyPlugin = require('copy-webpack-plugin');
+
 const nodeEnv = process.env.NODE_ENV;
 const production = nodeEnv && nodeEnv.indexOf('prod') > -1;
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 const config = {
-    resolve: {
-        modulesDirectories: ['node_modules', 'bower_components']
-    },
-    debug: true,
-    devtool: 'source-map',
-    entry: ['./src/entry.js'],
-    output: {
-        path: './dist',
-        filename: 'bundle.js',
-    },
-    module: {
-        loaders: [
-            {test: /\.png$/, loader: 'file?name=images/sprite.png'},
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
+  },
+  debug: true,
+  devtool: 'source-map',
+  entry: ['./src/entry.js'],
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.png$/, loader: 'file?name=images/sprite.png' },
+      { test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/ },
 
-            {
-                test: /\.(woff|woff2|eot|ttf|svg)$/,
-                loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'
-            }
-        ],
-    },
-    plugins: [
-        new webpack.ResolverPlugin(
-            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('.bower.json', ['main'])
-        ),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-        }),
-        new SpritesmithPlugin({
-            src: {
-                cwd: path.resolve(__dirname, 'src/images'),
-                glob: '*.png'
-            },
-            target: {
-                image: path.resolve(__dirname, 'src/images/build/sprite.png'),
-                css: path.resolve(__dirname, 'src/less/build/sprite.less')
-            },
-            apiOptions: {
-                cssImageRef: '~sprite.png'
-            }
-        }),
-        new CopyPlugin([
-            {from: './src/index.html'},
-            {from: './src/images/logo_gray-blue_80px.svg', to: 'images/logo_gray-blue_80px.svg'},
-        ]),
-        new ImageminPlugin({
-            disable: process.env.NODE_ENV !== 'prod',
-            pngquant: {
-                quality: '95-100'
-            }
-        }),
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=1024&name=fonts/[name].[ext]',
+      },
     ],
-    devServer: {
-        contentBase: './src'
-    }
+  },
+  plugins: [
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('.bower.json', ['main']),
+    ),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new SpritesmithPlugin({
+      src: {
+        cwd: path.resolve(__dirname, 'src/images'),
+        glob: '*.png',
+      },
+      target: {
+        image: path.resolve(__dirname, 'src/images/build/sprite.png'),
+        css: path.resolve(__dirname, 'src/less/build/sprite.less'),
+      },
+      apiOptions: {
+        cssImageRef: '~sprite.png',
+      },
+    }),
+    new CopyPlugin([
+      { from: './src/index.html' },
+      { from: './src/images/logo_gray-blue_80px.svg', to: 'images/logo_gray-blue_80px.svg' },
+    ]),
+    new ImageminPlugin({
+      disable: process.env.NODE_ENV !== 'prod',
+      pngquant: {
+        quality: '95-100',
+      },
+    }),
+  ],
+  devServer: {
+    contentBase: './src',
+  },
 };
 
 if (production) {
-    config.module.loaders.push({
-        test: /\.less$/,
-        loader: "style-loader!css-loader!csso-loader!autoprefixer-loader?browsers=last 2 version!less-loader"
-    });
+  config.module.loaders.push({
+    test: /\.less$/,
+    loader: 'style-loader!css-loader!csso-loader!autoprefixer-loader?browsers=last 2 version!less-loader',
+  });
 } else {
-    config.module.loaders.push({test: /\.less$/, loader: "style-loader!css-loader!less-loader"});
+  config.module.loaders.push({ test: /\.less$/, loader: 'style-loader!css-loader!less-loader' });
 }
 
 module.exports = config;
